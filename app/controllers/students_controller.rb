0e5fc13316bf75a11b9b93759ab3fea512391bc1
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-11 15:36:50 +0700

    Add Student model, controller, views.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,56 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: %i[ show edit update destroy ]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to student_path(@student), notice: "Student was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to student_path(@student), notice: "Student was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @student.destroy
+
+    respond_to do |format|
+      format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    def student_params
+      params.require(:student).permit(:name, :email, :birthday)
+    end
+end
---
+++ db/migrate/20230911082243_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :name, null: false, default: ''
+      t.string :email, null: false, default: ''
+      t.date :birthday
+
+      t.timestamps
+    end
+  end
+end
