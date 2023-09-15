8cc827d6621af8914276e96ab2cf8d6f04ab0b95
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-15 14:17:43 +0700

    Delete tests files and set app to stop auto-generating tests.

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Railsmvc
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.generators.test_framework nil
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
