458ca04f9245629996bb6c9e4a7d999a736ceae2
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-15 14:52:00 +0700

    set root for signed in users and customers.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :grades, module: :moderators
   resources :students, module: :moderators
+  authenticated :user do
+    root :to => 'moderators/students#index', as: :authenticated_moderator
+  end
+  authenticated :customer do
+    root :to => 'customers/customer_pages#index', as: :authenticated_customer
+  end
   root 'front/pages#index'
   get 'customer_index' => 'customers/customer_pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
