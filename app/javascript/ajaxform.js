0455f6e016fdd2c2b6c4b7ee8ebaf1a83c377ed9
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-22 12:03:01 +0700

    Re-work AJAX code.

--- app/javascript/ajaxform.js
+++ app/javascript/ajaxform.js
@@ -1,32 +1,28 @@
+async function updateSelectField(selectField) {
+  let selectedValue = selectField.value
+
+  try {
+    let data = await fetch('/student_classes/get_teachers').then(response => {
+      if (!response.ok) {
+        let errorMessage = `HTTP status: ${response.status}, ${response.statusText}`
+        throw new Error(errorMessage)
+      }
+      return response.json()
+    })
+
+    data.options.unshift({ value: 0, text: 'Select teacher' });
+
+    selectField.innerHTML = data.options.map(option => `<option value="${option.value}">${option.text}</option>`).join('')
+    selectField.value = selectedValue
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  const selectField = document.getElementById('dynamic-select');
+  let selectField = document.getElementById('dynamic-select')
 
   selectField.addEventListener('click', function() {
-    const selectedValue = selectField.value; // Store the currently selected value
-    // Send an AJAX request to fetch updated options
-    fetch('/student_classes/get_teachers')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const options = data.options;
-      selectField.innerHTML = ''; // Clear existing options
-      // Populate select field with updated options
-      options.forEach(option => {
-        const optionElement = document.createElement('option');
-        optionElement.value = option.value;
-        optionElement.textContent = option.text;
-        selectField.appendChild(optionElement);
-      });
-      // Re-select the previously selected value
-      selectField.value = selectedValue;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      // Handle the error here, e.g., show an alert or update a status message
-    });
-  });
-});
+    updateSelectField(selectField)
+  })
+})
